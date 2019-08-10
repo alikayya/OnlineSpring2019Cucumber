251c0c4e11503a61cbe4648007185737945b94d9
alikayya <alihikmetkaya@gmail.com>
2019-08-10 15:27:33 -0500

    first commit

---
+++ target/default-cucumber-reports/report.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src\\test\\resources\\features\\Login.feature");
+formatter.feature({
+  "name": "As user I want to login under different roles",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "Login as a driver",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@driver"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user is on the landing page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "LoginStepDefinitions.user_is_on_the_landing_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user logs in as a driver",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDefinitions.user_logs_in_as_a_driver()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user verifies that \"Dashboard\" page name is displayed",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "LoginStepDefinitions.user_verifies_that_page_name_is_displayed(String)"
+});
+formatter.result({
+  "error_message": "org.junit.ComparisonFailure: expected:\u003c[Dashboar]d\u003e but was:\u003c[Quick Launchpa]d\u003e\r\n\tat org.junit.Assert.assertEquals(Assert.java:115)\r\n\tat org.junit.Assert.assertEquals(Assert.java:144)\r\n\tat com.vytrack.step_definitions.LoginStepDefinitions.user_verifies_that_page_name_is_displayed(LoginStepDefinitions.java:33)\r\n\tat ✽.user verifies that \"Dashboard\" page name is displayed(src\\test\\resources\\features\\Login.feature:22)\r\n",
+  "status": "failed"
+});
+formatter.embedding("image/png", "embedded0.png");
+formatter.after({
+  "status": "passed"
+});
+});
